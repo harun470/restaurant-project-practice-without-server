90ecf75941758688490a6067dbff4b19f94fd730
Harun <harunalnayeem@gmail.com>
2022-01-10 16:29:18 +0600

    component tree done

---
+++ src/Components/mainComponents.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Header from './Header/Header'
+import Body from './Body/Body'
+import Footer from './Footer/Footer'
+
+ class mainComponents extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Body />
+                <Footer />
+                
+            </div>
+        )
+    }
+}
+export default mainComponents;
