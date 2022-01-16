c176950b8eadbba11580ec3645248ada73eeb388
Harun <harunalnayeem@gmail.com>
2022-01-16 14:49:13 +0600

    form created

--- src/Components/Body/Contact.js
+++ src/Components/Body/Contact.js
@@ -1,9 +1,105 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Formik } from 'formik'
 
-export default function Contact() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <Formik
+                    initialValues={{
+                        firstname:'',
+                        lastname:'',
+                        email:'',
+                        telnum:'',
+                        message:''
+                    }}
+                    onSubmit={
+                        (values)=>{
+                            console.log(values)
+                        }
+                    }
+                    validate={
+                        (values)=>{
+                            const errors={}
+                            if(!values.firstname){
+                                errors.firstname='required'
+                            }
+                            if(!values.lastname){
+                                errors.lastname='required'
+                            }
+                            if(!values.email){
+                                errors.email='required'
+                            }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)){
+                                errors.email="Invalid email"
+                            }
+                            if(!values.telnum){
+                                errors.telnum='required'
+                            }else if(values.telnum.length<11){
+                                errors.telnum='Number must be 11 digit'
+                            }
+                            if(!values.message){
+                                errors.message='required'
+                            }
+                            return errors
+                        }
+                    }
+                    
+                >
+                    {({values,handleChange,handleSubmit,errors})=>(
+                        <div style={{border:'1px solid grey',padding:'15px',borderRadius:'7px',margin:'50px'}}>
+                            <form onSubmit={handleSubmit}>
+                                <input 
+                                    name='firstname'
+                                    placeholder='Enter ur first name'
+                                    className='form-control'
+                                    value={values.firstname}
+                                    onChange={handleChange}
+                                />
+                                <span style={{color:'red'}}>{errors.firstname}</span>
+                                <br/>
+                                <input 
+                                    name='lastname'
+                                    placeholder='Enter ur last name'
+                                    className='form-control'
+                                    value={values.lastname}
+                                    onChange={handleChange}
+                                />
+                                <span style={{color:'red'}}>{errors.lastname}</span>
+                                <br/>
+                                <input 
+                                    name='email'
+                                    placeholder='Enter ur email'
+                                    className='form-control'
+                                    value={values.email}
+                                    onChange={handleChange}
+                                />
+                                <span style={{color:'red'}}>{errors.email}</span>
+                                <br/>
+                                <input 
+                                    name='telnum'
+                                    placeholder='Enter ur mobile number'
+                                    className='form-control'
+                                    value={values.telnum}
+                                    onChange={handleChange}
+                                />
+                                <span style={{color:'red'}}>{errors.telnum}</span>
+                                <br/>
+                                <textarea 
+                                    name='message'
+                                    placeholder='Enter ur message'
+                                    className='form-control'
+                                    value={values.message}
+                                    onChange={handleChange}
+                                />
+                                <span style={{color:'red'}}>{errors.message}</span>
+                                <br/>
+                                <button type='submit' className='btn btn-success'>Submit</button>
+                            </form>
+                        </div>
+                    )}
+                </Formik>
+            </div>
+        )
+    }
 }
+
--- src/Components/Body/Menu.js
+++ src/Components/Body/Menu.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react'
 import DISHES from '../../data/dishes'
 import MenuItem from './MenuItem';
 import MenudishDetail from './dishDetail';
+import {Button, CardColumns, Modal, ModalBody, ModalFooter} from 'reactstrap'
 
  class Menu extends Component {
      state={
          dishes: DISHES,
-         selectedDish:null
+         selectedDish:null,
+         modalOpen:false
      }
       dishSelect=(dish)=>{
         this.setState({
             selectedDish:dish
         })
+        this.toggleModal()
      }
-     
+     toggleModal=()=>{
+         this.setState({
+             modalOpen:!this.state.modalOpen
+         })
+     }
     render() {
         let menu= this.state.dishes.map(item=>{
             return (
@@ -29,12 +36,17 @@ import MenudishDetail from './dishDetail';
             <div>
                 <div className='container'>
                     <div className='row'>
-                        <div className='col-6'>
+                        <CardColumns>
                             {menu}
-                        </div>
-                        <div className='col-6'>
-                            {dishDetail}
-                        </div>
+                        </CardColumns>
+                        <Modal isOpen={this.state.modalOpen}>
+                            <ModalBody>
+                                {dishDetail}
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button onClick={this.toggleModal}>Close</Button>
+                            </ModalFooter>
+                        </Modal>
                     </div>
                 </div>
             </div>
